59395d82993901ab8e3aa65e7da63a7b6bf1fc9f
binaryleo <leomouracss@fastmail.com>
2022-08-14 13:48:35 -0300

    chore: add functions file

--- fundamentals/functions.kt
+++ fundamentals/functions.kt
@@ -1,5 +1,13 @@
 package fundamentals
 
+// Functions: A set of instructions bundled together to achieve a certain outcome
 fun main(){
-
+sayHello()
+sum(1,2)
+}
+fun sayHello(){
+    println("Hello!")
+}
+fun sum(a: Int,b: Int){ // simple function using parameters
+    println(a+b);
 }
