d79b64005631e577dac84714383b81e3d02ea238
binaryleo <leomouracss@fastmail.com>
2022-08-14 10:25:42 -0300

    chore update loop file

--- loops.kt
+++ loops.kt
@@ -2,9 +2,11 @@ fun main (){
    /* Loops  and branching
     if amount is equal to 1000, then you're wealthy, else you are getting by just fine
     */
-    val amount = 900
-    if(amount == 1000){
+    val amount = 1001
+    if(amount == 1000) {
         println("you're wealthy")
+    }else if(amount > 1000){
+        println("Wow .. you are very wealthy")
     }else{
         println("you are getting by just fine")
     }
