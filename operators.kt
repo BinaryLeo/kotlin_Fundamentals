9e21af4f8c6a0f1ea0372b1441c5a7be7140cc6d
binaryleo <leomouracss@fastmail.com>
2022-08-14 10:05:54 -0300

    chore: add operators file

---
+++ operators.kt
@@ -0,0 +1,15 @@
+fun main(){
+    // Kotlin operators
+    val a = 23.0
+    val b = 12.0
+    println(a + b)// 35.0
+    println(a - b) //11.0
+    println(a / b) // 1.9166666666666667
+
+    val c = 10
+    val d = 2
+    val e = 3
+    println(c % d) // 0  is the remainder
+    println(c%e)// 1 is the remainder
+    println(c * d) // 20
+}
