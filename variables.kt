2c9db877021a05db6ca96a92977c5101bc39a629
binaryleo <leomouracss@fastmail.com>
2022-08-14 09:52:09 -0300

    chore add variables.kt

--- variables.kt
+++ variables.kt
@@ -15,10 +15,33 @@ fun main(){
 
     //Variables types
     val user = "James" //String
-    val Age = 38 //Int
-    val temperature =  28.5
+    println("user has a " + user.javaClass.kotlin.simpleName.toString().lowercase() + " type!")
+    println("Age has a " + age.javaClass.kotlin.simpleName.toString().lowercase() + " type!")
+
+    //FLoat - Decimal numbers. Holds 32 bits
+    //Double - Holds 64 bits (Hold  more precision than 32 bits)
+    val p = 3.14159
+    val temperature = 28.5f
 
     //Get types
     println("Temperature has a " + temperature.javaClass.kotlin.simpleName.toString().lowercase() + " type!")
-    println("user has a " + user.javaClass.kotlin.simpleName.toString().lowercase() + " type!")
+    println("p or pi has a " + p.javaClass.kotlin.simpleName.toString().lowercase() + " type!")
+
+    /*
+    types and sizes : small | medium | large | x-large
+    Int - A whole number . Holds 32 bits
+    Byte holds 8 bits
+    Short holds 16 bits
+    Long holds 64 bits
+     */
+
+    val myByte: Byte = 127
+    //myByte = 128 -> the integer literal does not conform to the expected type Byte
+
+    val number = 1000000000000000000
+    val oneLong = 1L
+    println("oneLong has a " + oneLong.javaClass.kotlin.simpleName.toString().lowercase() + " type!")
+
+
+
 }
