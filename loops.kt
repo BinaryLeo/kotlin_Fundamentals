5d145daa3d5551c1747febb38813f4e2347b4807
binaryleo <leomouracss@fastmail.com>
2022-08-14 13:14:58 -0300

    chore: when expressions

--- loops.kt
+++ loops.kt
@@ -5,9 +5,24 @@ fun main (){
     val amount = 1001
     if(amount == 1000) {
         println("you're wealthy")
-    }else if(amount > 1000){
+    }else if(amount >= 1000){
         println("Wow .. you are very wealthy")
     }else{
         println("you are getting by just fine")
+    }
+    //When expression
+    when(amount){
+        100-> println("you have 100")
+        125-> println("you are getting there..")
+
+        in 300..380 -> println("you have a great progress ..")
+        !in 300..380 -> println("you need to work more than now")
+
+        999-> println("Really close")
+        1000-> println("Rich but not there")
+        1100 -> println("you've made it")
+        else ->{
+            println("$amount is just not going to work")
+        }
     }
 }
