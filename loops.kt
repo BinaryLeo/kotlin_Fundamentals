39004b963a2049f2696c868a7189f1e155affd5e
binaryleo <leomouracss@fastmail.com>
2022-08-14 10:23:52 -0300

    chore: add loops file

---
+++ loops.kt
@@ -0,0 +1,11 @@
+fun main (){
+   /* Loops  and branching
+    if amount is equal to 1000, then you're wealthy, else you are getting by just fine
+    */
+    val amount = 900
+    if(amount == 1000){
+        println("you're wealthy")
+    }else{
+        println("you are getting by just fine")
+    }
+}
