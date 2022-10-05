492d45289352d07f402db34ece6b13966d6cbe80
binaryleo <leomouracss@fastmail.com>
2022-10-05 10:39:20 -0300

    chore: return types

--- fundamentals/functions.kt
+++ fundamentals/functions.kt
@@ -4,7 +4,17 @@ package fundamentals
 fun main() {
     sayHello()
     sum(1, 2)
-    calculate(11,82)
+    calculate(
+        11,
+        89,
+        evenMessage = "is a multiple of",
+        oddMessage = "is not a multiple of",
+        multipleOf = 11
+    )
+
+    println( "Your cat has " +calculateCatAge(2) + " yrs old.")
+    val catAge = calculateCatAge(age = 12)
+    println( "Your cat has $catAge yrs old.")
 }
 
 fun sayHello() {
@@ -15,14 +25,26 @@ fun sum(a: Int, b: Int) { // simple function using parameters
     println(a + b);
 }
 
-fun calculate(first: Int, second: Int) {
+fun calculate(
+    first: Int,
+    second: Int,
+    evenMessage: String,
+    oddMessage: String,
+    multipleOf: Int
+
+) {
     for (i in first..second) {
-        if (i % 2 == 0) {
-            println("$i is multiple of 2")
+        if (i % multipleOf == 0) {
+            println("$i $evenMessage $multipleOf")
         } else {
-            println("$i is not multiple of 2")
+            println("$i $oddMessage $multipleOf")
         }
 
 
     }
+}
+
+fun calculateCatAge(age:Int):Int{
+    return age * 7
+
 }
