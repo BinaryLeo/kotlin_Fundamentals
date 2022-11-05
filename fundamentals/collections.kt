1b38edbce36879ae132d69a785c73b20a6f677af
BinaryLeo <leomouracss@fastmail.com>
2022-11-05 02:27:01 -0300

    chore: add collections.kt file

--- fundamentals/collections.kt
+++ fundamentals/collections.kt
@@ -1,5 +1,12 @@
 package fundamentals
 
 fun main(){
+// A collection usually contains a number of objects/items in a certain order of the same type.
+//List / ArrayList/ ListOf()
 
+val myListOfNames = listOf("Leonardo", "Luigi","Matteo")
+    println(myListOfNames[1])
+    for(item in myListOfNames){
+        println("$item you are the index " + myListOfNames.indexOf(item) + " of our developers list" )
+    }
 }
